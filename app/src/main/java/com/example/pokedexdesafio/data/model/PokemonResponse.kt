e810b1dc288e7b49c05ad8288483e18377909e1c
Hector A. Narvaez <hanarvaez92@gmail.com>
2022-12-22 18:49:01 -0500

    Added extension functions for map data models into business models

--- app/src/main/java/com/example/pokedexdesafio/data/model/PokemonResponse.kt
+++ app/src/main/java/com/example/pokedexdesafio/data/model/PokemonResponse.kt
@@ -1,11 +1,25 @@
 package com.example.pokedexdesafio.data.model
 
 import android.os.Parcelable
+import com.example.pokedexdesafio.domain.model.Pokemon
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class PokemonContainerResponse(val count: Int, val next: String, val results: List<PokemonResponse>) :
-    Parcelable
+data class PokemonContainerResponse(
+    val count: Int,
+    val next: String,
+    val results: List<PokemonResponse>
+) : Parcelable
 
 @Parcelize
 data class PokemonResponse(val id: Int?, val name: String?, val url: String?) : Parcelable
+
+// Ext functions for mapping into business models
+fun PokemonResponse.toPokemon(): Pokemon? =
+    if (id == null || name.isNullOrBlank() || url.isNullOrBlank()) {
+        null
+    } else {
+        Pokemon(id, name, url)
+    }
+
+fun PokemonContainerResponse.toPokemonList(): List<Pokemon> = results.mapNotNull { it.toPokemon() }
