55b1a2060275ea78aa98ecc2f57076d3c2a18539
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2022-12-23 12:48:05 -0500

    id pokemon entity optional

--- app/src/main/java/com/example/pokedexdesafio/domain/entities/PokemonEntity.kt
+++ app/src/main/java/com/example/pokedexdesafio/domain/entities/PokemonEntity.kt
@@ -9,7 +9,7 @@ import com.example.pokedexdesafio.domain.model.TypeContainer
 
 @Entity
 data class PokemonEntity (
-    @PrimaryKey val id: Int?,
+    @PrimaryKey val id: Int,
     @ColumnInfo(name= "name") val name: String?,
     @ColumnInfo(name= "types") val types: List<String?>,
     @ColumnInfo(name= "moves") val moves: List<MoveContainer?>,
