ee80b855ac63d9ec5f7387571d18a7b581acdea5
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2022-12-24 16:44:47 -0500

    change extension functions to convert from data model to business model

--- app/src/main/java/com/example/pokedexdesafio/data/model/PokemonDetailResponse.kt
+++ app/src/main/java/com/example/pokedexdesafio/data/model/PokemonDetailResponse.kt
@@ -1,13 +1,7 @@
 package com.example.pokedexdesafio.data.model
 
 import android.os.Parcelable
-import com.example.pokedexdesafio.domain.model.MoveDetail
-import com.example.pokedexdesafio.domain.model.PokemonDetail
-import com.example.pokedexdesafio.domain.model.TypeDetail
-import com.example.pokedexdesafio.domain.model.TypeList
-import com.example.pokedexdesafio.domain.model.MoveList
-import com.example.pokedexdesafio.domain.model.AbilityDetail
-import com.example.pokedexdesafio.domain.model.AbilityList
+import com.example.pokedexdesafio.domain.model.*
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -42,42 +36,89 @@ data class AbilityContainer(val ability: Ability?, val isHidden: Boolean?, val s
 @Parcelize
 data class Ability(val name: String?, val url: String?) : Parcelable
 
-
-fun PokemonDetailResponse.toPokemonDetail(): PokemonDetail? =
-    if (name.isNullOrBlank() || types.isEmpty() || moves.isEmpty() || abilities.isEmpty() || locationAreaEncounters.isNullOrBlank()) {
+// Ext functions for mapping into business models
+fun Ability.toAbility(): AbilityDetail? =
+    if (name.isNullOrBlank() || url.isNullOrBlank()) {
         null
     } else {
-
-        var pokemonDetailTypeList: TypeList
-        var typePokemonDetail: TypeDetail
-        var listTypeList: MutableList<TypeList>
-        types.forEach {
-            if(it?.type != null){
-                typePokemonDetail = TypeDetail(it.type.name, it.type.url)
-                pokemonDetailTypeList = TypeList(it.slot, typePokemonDetail)
-                listTypeList.add(pokemonDetailTypeList)
-            }
-        }
-        var moveDetail: MoveDetail
-        var listMoveDetail: MoveList
-        var movesList: MutableList<MoveList>
-        moves.forEach {
-            if(it?.move != null){
-                moveDetail = MoveDetail(it.move.name, it.move.url)
-                listMoveDetail = MoveList(moveDetail)
-                movesList.add(listMoveDetail)
-            }
-        }
-        var abilityDetail: AbilityDetail
-        var listAbilityList: AbilityList
-        var abilityList: MutableList<AbilityList>
-        abilities.forEach {
-            if (it?.ability!=null){
-                abilityDetail = AbilityDetail(it.ability.name, it.ability.url)
-                listAbilityList = AbilityList(abilityDetail, it.isHidden, it.slot)
-                abilityList.add(listAbilityList)
-            }
-        }
-
-        PokemonDetail(id, name, listTypeList, movesList,abilityList, locationAreaEncounters)
+        AbilityDetail(name, url)
     }
+
+fun AbilityContainer.toAbilityList(): AbilityList? =
+    if(ability != null || isHidden != null || slot != null){
+        AbilityList(ability?.toAbility(), isHidden, slot)
+    }else{
+        null
+    }
+fun PokemonDetailResponse.toAbilitiesList(): List<AbilityList> = abilities.mapNotNull { it?.toAbilityList() }
+
+fun Move.toMove(): MoveDetail? =
+    if (name.isNullOrBlank() || url.isNullOrBlank()) {
+        null
+    } else {
+        MoveDetail(name, url)
+    }
+
+fun MoveContainer.toMoveList(): MoveList? =
+    if(move != null){
+        MoveList(move.toMove())
+    }else{
+        null
+    }
+
+fun PokemonDetailResponse.toMovesList(): List<MoveList> = moves.mapNotNull { it?.toMoveList() }
+
+fun Type.toType(): TypeDetail? =
+    if (name.isNullOrBlank() || url.isNullOrBlank()) {
+        null
+    } else {
+        TypeDetail(name, url)
+    }
+
+fun TypeContainer.toTypeList(): TypeList? =
+    if(type != null && slot !=null){
+        TypeList(slot, type.toType())
+    }else{
+        null
+    }
+
+fun PokemonDetailResponse.toTypesList(): List<TypeList> = types.mapNotNull { it?.toTypeList() }
+
+//fun PokemonDetailResponse.toPokemonDetail(): PokemonDetail? =
+//    if (name.isNullOrBlank() || types.isEmpty() || moves.isEmpty() || abilities.isEmpty() || locationAreaEncounters.isNullOrBlank()) {
+//        null
+//    } else {
+//
+//        var pokemonDetailTypeList: TypeList
+//        var typePokemonDetail: TypeDetail
+//        var listTypeList: MutableList<TypeList>
+//        types.forEach {
+//            if(it?.type != null){
+//                typePokemonDetail = TypeDetail(it.type.name, it.type.url)
+//                pokemonDetailTypeList = TypeList(it.slot, typePokemonDetail)
+//                listTypeList.add(pokemonDetailTypeList)
+//            }
+//        }
+//        var moveDetail: MoveDetail
+//        var listMoveDetail: MoveList
+//        var movesList: MutableList<MoveList>
+//        moves.forEach {
+//            if(it?.move != null){
+//                moveDetail = MoveDetail(it.move.name, it.move.url)
+//                listMoveDetail = MoveList(moveDetail)
+//                movesList.add(listMoveDetail)
+//            }
+//        }
+//        var abilityDetail: AbilityDetail
+//        var listAbilityList: AbilityList
+//        var abilityList: MutableList<AbilityList>
+//        abilities.forEach {
+//            if (it?.ability!=null){
+//                abilityDetail = AbilityDetail(it.ability.name, it.ability.url)
+//                listAbilityList = AbilityList(abilityDetail, it.isHidden, it.slot)
+//                abilityList.add(listAbilityList)
+//            }
+//        }
+//
+//        PokemonDetail(id, name, listTypeList, movesList,abilityList, locationAreaEncounters)
+//    }
