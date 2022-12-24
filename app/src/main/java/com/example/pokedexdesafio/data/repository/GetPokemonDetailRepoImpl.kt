06cc760b1f86614a6ff1850160fc1b3765a19c97
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2022-12-24 17:12:36 -0500

    change parameters from data model to business model in repo, service and activity

--- app/src/main/java/com/example/pokedexdesafio/data/repository/GetPokemonDetailRepoImpl.kt
+++ app/src/main/java/com/example/pokedexdesafio/data/repository/GetPokemonDetailRepoImpl.kt
@@ -2,6 +2,7 @@ package com.example.pokedexdesafio.data.repository
 
 import com.example.pokedexdesafio.data.services.GetPokemonDetailService
 import com.example.pokedexdesafio.data.model.PokemonDetailResponse
+import com.example.pokedexdesafio.domain.model.PokemonDetail
 import com.example.pokedexdesafio.domain.repository.GetPokemonDetailsRepo
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -11,7 +12,7 @@ import javax.inject.Inject
 class GetPokemonDetailRepoImpl @Inject constructor(private val pokemonDetailService: GetPokemonDetailService) :
     GetPokemonDetailsRepo {
 
-    override fun getPokemonDetails(id: String): Single<Response<PokemonDetailResponse>> =
+    override fun getPokemonDetails(id: String): Single<Response<PokemonDetail>> =
         Single.fromCallable { pokemonDetailService.fetchPokemonDetails(id) }
             .subscribeOn(Schedulers.io())
 }
