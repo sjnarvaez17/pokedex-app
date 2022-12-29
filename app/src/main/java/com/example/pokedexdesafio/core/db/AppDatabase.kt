ec6077a1a5fad63d483593cfcb1ff5c93e357738
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2022-12-28 19:09:34 -0500

    create app database

---
+++ app/src/main/java/com/example/pokedexdesafio/core/db/AppDatabase.kt
@@ -0,0 +1,12 @@
+package com.example.pokedexdesafio.core.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.pokedexdesafio.data.dao.PokemonDao
+import com.example.pokedexdesafio.data.entities.PokemonEntity
+
+@Database(entities = [PokemonEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun pokemonDao(): PokemonDao
+}
