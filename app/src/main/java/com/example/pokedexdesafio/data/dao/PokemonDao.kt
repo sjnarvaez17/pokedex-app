7050508f8dd7003f684ca26fcce1fccd5dfb57d8
Santiago Narvaez <santi_narvaez_98@hotmail.com>
2023-01-02 09:08:24 -0500

    add insert function in DAO

--- app/src/main/java/com/example/pokedexdesafio/data/dao/PokemonDao.kt
+++ app/src/main/java/com/example/pokedexdesafio/data/dao/PokemonDao.kt
@@ -1,7 +1,9 @@
 package com.example.pokedexdesafio.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import com.example.pokedexdesafio.data.entities.PokemonEntity
 import com.example.pokedexdesafio.domain.model.Pokemon
 
 
@@ -10,4 +12,7 @@ interface PokemonDao {
 
     @Query("SELECT * FROM pokemon_entity")
     fun getAllPokemons(): List<Pokemon>
+
+    @Insert
+    fun insertPokemon(newPokemon: PokemonEntity)
 }
